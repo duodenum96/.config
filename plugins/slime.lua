ba0d89e643f3ae659228468e8b51c89ac4dc1930
Yasir Çatal <catalyasir@gmail.com>
2026-01-30 17:33:32 +0300

    dont preserve cursorpos in slime

--- plugins/slime.lua
+++ plugins/slime.lua
@@ -6,7 +6,7 @@ return {
   config = function()
     vim.g.slime_target = "neovim"
     vim.g.slime_dont_ask_default = 1
-    vim.g.slime_preserve_curpos = 1
+    vim.g.slime_preserve_curpos = 0
     vim.g.slime_no_mappings = 1
     vim.g.slime_neovim_ignore_unlisted = 1
     vim.b.slime_bracketed_paste = 1
